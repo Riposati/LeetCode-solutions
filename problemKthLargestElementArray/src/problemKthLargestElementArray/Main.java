f997f99d482f653217357d3e639813ac3342f992
Gustavo <guriposa@gmail.com>
2022-11-15 16:48:08 -0300

    new solutions

---
+++ problemKthLargestElementArray/src/problemKthLargestElementArray/Main.java
@@ -0,0 +1,47 @@
+package problemKthLargestElementArray;
+
+import java.io.IOException;
+import java.util.PriorityQueue;
+
+class Solution {
+	public int findKthLargest(int[] x, int k) {
+
+		PriorityQueue<Integer>pq = new PriorityQueue<>();
+		
+		for(int i=0;i<k;i++) {
+			pq.add(x[i]);
+		}
+		
+		for(int i=k;i<x.length;i++) {
+			
+			if(pq.peek() < x[i]) {
+				pq.poll();
+				pq.add(x[i]);
+			}
+		}
+		
+		return pq.peek();
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+//		InputStreamReader ir = new InputStreamReader(System.in);
+//		BufferedReader in = new BufferedReader(ir);
+
+//		double a;
+//		int b;
+//
+//		a = Double.parseDouble(in.readLine());
+//		b = Integer.parseInt(in.readLine());
+
+		Solution s = new Solution();
+
+		int[] x = { 3,2,1,5,6,4 };
+
+		System.out.println(s.findKthLargest(x, 2));
+	}
+
+}
---
+++ validPalindrome/src/validPalindrome/Main.java
@@ -0,0 +1,37 @@
+package validPalindrome;
+
+class Solution {
+	public boolean isPalindrome(String s) {
+		
+		String str = s.replaceAll("[^a-zA-Z0-9]", "");
+
+		StringBuilder sb = new StringBuilder(str);
+
+//		System.out.println(sb);
+//		System.out.println(sb.reverse());
+		
+		String aux = sb.toString().toLowerCase();
+		String aux2 = sb.reverse().toString().toLowerCase();
+		
+		for(int i=0;i<sb.length();i++) {
+			if(aux.charAt(i)!=aux2.charAt(i)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String aux = "A man, a plan, a canal: Panama";
+
+		Solution s = new Solution();
+		
+		System.out.println(s.isPalindrome(aux));
+	}
+
+}
